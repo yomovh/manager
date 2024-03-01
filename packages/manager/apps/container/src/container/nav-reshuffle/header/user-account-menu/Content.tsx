d47be6878da3fad63c29ed272dbcd61575b21bae
benjaminlepine <benjamin.lepine@hotmail.fr>
2024-03-01 19:38:22 +0000

    feat(container): pnrv2 user widget
    ref: MANAGER-13168
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

--- packages/manager/apps/container/src/container/nav-reshuffle/header/user-account-menu/Content.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/header/user-account-menu/Content.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import { useTranslation, Trans } from 'react-i18next';
 
-import icon from './assets/give_feedback.png';
 import UserDefaultPaymentMethod from './DefaultPaymentMethod';
 import style from './style.module.scss';
 
@@ -23,9 +21,7 @@ const UserAccountMenu = ({
   const trackingPlugin = shell.getPlugin('tracking');
   const environment = shell.getPlugin('environment').getEnvironment();
   const region = environment.getRegion();
-
-  const { getFeedbackUrl, closeAccountSidebar } = useProductNavReshuffle();
-  const feedbackUrl = getFeedbackUrl();
+  const { closeAccountSidebar } = useProductNavReshuffle();
 
   const user = shell
     .getPlugin('environment')
@@ -36,7 +32,7 @@ const UserAccountMenu = ({
 
   const displayUserName = {
     userName: isSubUser ? user.auth.user : `${user.firstname} ${user.name}`,
-    className: `oui-heading_4 mb-1 ${isSubUser && 'text-truncate'}`
+    className: `oui-heading_4 mb-1 ${isSubUser && 'text-truncate'}`,
   };
 
   const onLougoutBtnClick = () => {
@@ -47,14 +43,6 @@ const UserAccountMenu = ({
     shell.getPlugin('auth').logout();
   };
 
-  const onGiveFeedbackLinkClick = () => {
-    closeAccountSidebar();
-    trackingPlugin.trackClick({
-      name: 'topnav::user_widget::give_feedback',
-      type: 'action',
-    });
-  };
-
   const onMyAccountLinkClick = () => {
     closeAccountSidebar();
     trackingPlugin.trackClick({
@@ -63,111 +51,120 @@ const UserAccountMenu = ({
     });
   };
 
-  const myAccountLink = shell
-    .getPlugin('navigation')
-    .getURL('dedicated', '#/useraccount/dashboard');
+  const getUrl = (key: string, hash: string) =>
+    shell.getPlugin('navigation').getURL(key, hash);
+
+  const myAccountLink = getUrl('dedicated', '#/useraccount/dashboard');
+  const myInvoicesLink = getUrl('dedicated', '#/billing/history');
+  const myCommunicationsLink = getUrl('dedicated', '#/useraccount/emails');
+  const myContractsLink = getUrl('dedicated', '#/billing/autorenew');
 
   return (
     <div className={`${style.menuContent} oui-navbar-menu__wrapper`}>
       <div
-        className="oui-navbar-menu oui-navbar-menu_fixed oui-navbar-menu_end p-3"
+        className="oui-navbar-menu oui-navbar-menu_fixed oui-navbar-menu_end p-4"
         data-navi-id="account-sidebar-block"
       >
-        <h1 className={displayUserName.className}>{displayUserName.userName}</h1>
-        {['EU', 'CA'].includes(region) && (
-          <p className="oui-chip mb-0">
-            <strong className={style.supportLevel}>
-              {t(
-                `user_account_menu_support_level_${user.supportLevel.level}${user.isTrusted ? '_trusted' : ''
-                }`,
-              )}
-            </strong>
-          </p>
-        )}
-        <p className="mb-0" data-navi-id="account-email">
-          <Trans
-            t={t}
-            i18nKey="user_account_menu_notification_email_strong"
-            values={{ email: user.email }}
+        <div className="border-bottom pb-2 pt-2">
+          <h1 className={displayUserName.className}>
+            {displayUserName.userName}
+          </h1>
+          <a
+            onClick={onMyAccountLinkClick}
+            className="d-block my-3"
+            aria-label={t('user_account_menu_profile')}
+            title={t('user_account_menu_profile')}
+            href={myAccountLink}
+            target="_top"
+            id="user-account-menu-profile"
+            data-navi-id="profile"
           >
-          </Trans>
-        </p>
-        {user.email !== user.nichandle && (
-          <p className="mb-0">
-            <Trans
-              t={t}
-              i18nKey="user_account_menu_user_id"
-              values={{ nichandle: user.nichandle }}
-            ></Trans>
+            {t('user_account_menu_profile')}
+          </a>
+          <p className="mb-0" data-navi-id="account-email">
+            {user.email}
           </p>
-        )}
-
-        <p className={`ml-0 oui-badge oui-badge_warning`} data-navi-id="account-auth-method">
-          <strong>{t(`user_account_menu_role_${user.auth.method}`)}</strong>
-        </p>
-
-        {!user.enterprise && (
-          <UserDefaultPaymentMethod
-            defaultPaymentMethod={defaultPaymentMethod}
-            isLoading={isLoading}
-          />
-        )}
-        <hr />
-        <a
-          href={feedbackUrl}
-          className={`${style.feedback} d-flex`}
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={t('user_account_menu_beta_feedback')}
-          onClick={onGiveFeedbackLinkClick}
-        >
-          <span className="align-self-center">
-            <img src={icon} alt={t('user_account_menu_beta_feedback')} />
-          </span>
-          <span className="pl-2">
-            <span className={`${style.feedback_title} d-block oui-link_icon`}>
-              <span>{t('user_account_menu_beta_feedback')}</span>
-              <span
-                className="oui-icon oui-icon-arrow-right ml-2"
-                aria-hidden="true"
-              ></span>
-            </span>
-            <span className={`${style.feedback_text} d-block`}>
-              {t('user_account_menu_beta_feedback_text')}
-            </span>
-          </span>
-        </a>
-        <hr />
-        <a
-          onClick={onMyAccountLinkClick}
-          className="d-block oui-link_icon"
-          aria-label={t('user_account_menu_profile')}
-          title={t('user_account_menu_profile')}
-          href={myAccountLink}
-          target="_top"
-          id="user-account-menu-profile"
-          data-navi-id="profile"
-        >
-          {t('user_account_menu_profile')}
-          <span
-            className={`${style.myAccountIcon} oui-icon oui-icon-arrow-right`}
-            aria-hidden="true"
-          ></span>
-        </a>
+          {user.email !== user.nichandle && (
+            <p className="mb-0">
+              <Trans
+                t={t}
+                i18nKey="user_account_menu_user_id"
+                values={{ nichandle: user.nichandle }}
+              ></Trans>
+            </p>
+          )}
+        </div>
+        <div className="border-bottom pb-2 pt-2">
+          <div className="d-flex justify-content-between">
+            <span>{t('user_account_menu_role_connexion')}</span>
+            <p
+              className={`ml-0 oui-chip oui-badge_warning mb-0`}
+              data-navi-id="account-auth-method"
+            >
+              <strong>{t(`user_account_menu_role_${user.auth.method}`)}</strong>
+            </p>
+          </div>
+          {!user.enterprise && (
+            <UserDefaultPaymentMethod
+              defaultPaymentMethod={defaultPaymentMethod}
+              isLoading={isLoading}
+            />
+          )}
+          <div className="d-flex mt-1 justify-content-between">
+            <span>{t('user_account_menu_support')}</span>
+            {['EU', 'CA'].includes(region) && (
+              <p className="oui-chip oui-badge_info mb-0">
+                {t(
+                  `user_account_menu_support_level_${user.supportLevel.level}${
+                    user.isTrusted ? '_trusted' : ''
+                  }`,
+                )}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="border-bottom pb-2 pt-2">
+          <a
+            onClick={closeAccountSidebar}
+            className="d-block"
+            aria-label={t('user_account_menu_my_invoices')}
+            title={t('user_account_menu_my_invoices')}
+            href={myInvoicesLink}
+            target="_top"
+          >
+            {t('user_account_menu_my_invoices')}
+          </a>
+          <a
+            onClick={closeAccountSidebar}
+            className="d-block"
+            aria-label={t('user_account_menu_my_communication')}
+            title={t('user_account_menu_my_communication')}
+            href={myCommunicationsLink}
+            target="_top"
+          >
+            {t('user_account_menu_my_communication')}
+          </a>
+          <a
+            onClick={closeAccountSidebar}
+            className="d-block"
+            aria-label={t('user_account_menu_my_contracts')}
+            title={t('user_account_menu_my_contracts')}
+            href={myContractsLink}
+            target="_top"
+          >
+            {t('user_account_menu_my_contracts')}
+          </a>
+        </div>
         <button
           type="button"
           role="button"
-          className="w-100 text-left oui-button oui-button_icon-right oui-button_link px-0"
+          className="w-100 oui-button oui-button_link mt-3 center"
           onClick={onLougoutBtnClick}
           aria-label={t('user_account_menu_logout')}
           title={t('user_account_menu_logout')}
           data-navi-id="logout"
         >
           {t('user_account_menu_logout')}
-          <span
-            className="oui-icon oui-icon-arrow-right"
-            aria-hidden="true"
-          ></span>
         </button>
       </div>
     </div>
